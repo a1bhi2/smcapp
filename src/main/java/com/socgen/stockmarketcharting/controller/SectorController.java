ecad9d89c5f5f7ac3413b9c0a498e22d114e5d47
a1bhi2 <agaguptaji@gmail.com>
2021-06-22 09:59:23 +0530

    make it better

--- src/main/java/com/socgen/stockmarketcharting/controller/SectorController.java
+++ src/main/java/com/socgen/stockmarketcharting/controller/SectorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/sector")
 public class SectorController {
